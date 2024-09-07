60fcf7722fb596bbc383d0e17c4e4e43f78a8d20
“KamalKumarSahu554” <kamalkumar554@outlook.com>
2024-09-07 14:34:12 +0530

    03. Pattern

---
+++ 03. Pattern/patterns.cpp
@@ -0,0 +1,159 @@
+/*
+# Re-visit this pattern to solve other problems#
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter a number : ";
+  cin >> n;
+
+  // // Pattern 1
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   for (int j = 1; j <= n; j++)
+  //   {
+  //     cout << i << " ";
+  //   }
+  //   cout << "\n";
+  // }
+
+  // // Pattern 2
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   for (int j = 1; j <= n; j++)
+  //   {
+  //     cout << j << " ";
+  //   }
+  //   cout << "\n";
+  // }
+
+  // // Pattern 3
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   for (int j = 1; j <= n; j++)
+  //   {
+  //     cout << "*" << " ";
+  //   }
+  //   cout << "\n";
+  // }
+
+  // // Patern 4
+  // for (int i = 0; i < n; i++)
+  // {
+  //   char ch = 'A';
+  //   for (int i = 0; i < n; i++)
+  //   {
+  //     cout << ch << " ";
+  //     ch++;
+  //   }
+  //   cout << "\n";
+  // }
+
+  // // Patern 5
+  // for (int i = 0; i < n; i++)
+  // {
+  //   char chr = 'A';
+  //   for (int i = 0; i < n; i++)
+  //   {
+  //     cout << chr << " ";
+  //     chr++;
+  //   }
+  //   cout << "\n";
+  // }
+
+  // // Pattern 6
+  // int num = 1;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < n; j++)
+  //   {
+  //     cout << num << " ";
+  //     num++;
+  //   }
+  //   cout << endl;
+  // }
+
+  // // Pattern 7
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  //   ;
+  // }
+
+  // // Pattern 8
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << i + 1 << " ";
+  //       }
+  //   cout << endl;
+  //   ;
+  // }
+
+  // // Pattern 9
+  // char ch = 'A';
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << ch << " ";
+  //   }
+  //   ch++;
+  //   cout << endl;
+  //   ;
+  // }
+
+  // // Pattern 10
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << j + 1 << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // // Pattern 11
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = i + 1; j > 0; j--)
+  //   {
+  //     cout << j << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // // Pattern 12
+  // int num = 1;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << num << " ";
+  //     num++;
+  //   }
+  //   cout << endl;
+  // }
+
+  // // Pattern 13
+  // char ch = 'A';
+  // for (int i = 0; i < n; i++)
+  // {
+  //   for (int j = 0; j < i + 1; j++)
+  //   {
+  //     cout << ch << " ";
+  //     ch++;
+  //   }
+  //   cout << endl;
+  // }
+  return 0;
+}
